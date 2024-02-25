cae1f2b45a29b28c26bbd48de7df468cb322bb0d
Vinicius Paiva <vinicius.arantespaiva@gmail.com>
2024-02-24 22:05:13 -0300

    Criando hook para consumir API

--- src/components/VehicleList.tsx
+++ src/components/VehicleList.tsx
@@ -4,30 +4,17 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
+import useRequest from "../hooks/useRequest";
+
 import CarInterface from "../interfaces/Vehicle";
 import VehicleListCard from "./VehicleListCard";
 import VehicleForm from "./VehicleForm";
 
 const VehicleList = () => {
+  const url = "http://localhost:3000/vehicles";
+  const { data: vehicles } = useRequest(url);
+
   const [isInsert, setIsInsert] = useState<boolean>(false);
-  const carList: CarInterface[] = [
-    {
-      id: 1,
-      brand: "Chevrolet",
-      model: "Onix",
-      color: "Preto",
-      year: 2015,
-      km: 118000,
-    },
-    {
-      id: 3,
-      brand: "Fiat",
-      model: "Argo",
-      color: "Branco",
-      year: 2020,
-      km: 45000,
-    },
-  ];
 
   return (
     <div className="container">
@@ -37,17 +24,19 @@ const VehicleList = () => {
       </button>
       {isInsert && <VehicleForm setIsInsert={setIsInsert} />}
       <div className="car-list">
-        {carList.map((car: CarInterface) => (
-          <VehicleListCard
-            id={car.id}
-            model={car.model}
-            brand={car.brand}
-            color={car.color}
-            year={car.year}
-            km={car.km}
-          />
-        ))}
-        {carList.length === 0 && (
+        {vehicles &&
+          vehicles.map((car: CarInterface) => (
+            <VehicleListCard
+              key={car.id}
+              id={car.id}
+              model={car.model}
+              brand={car.brand}
+              color={car.color}
+              year={car.year}
+              km={car.km}
+            />
+          ))}
+        {vehicles && vehicles.length === 0 && (
           <span className="empty-list">Nenhum carro registrado.</span>
         )}
       </div>
---
+++ src/hooks/useRequest.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import Vehicle from "../interfaces/Vehicle";
+
+const useRequest = (url: string) => {
+  const [data, setData] = useState<Vehicle[]>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+  const [callFetch, setCallFetch] = useState<boolean>(false);
+
+  const getHttpOptions = () => {
+    return {
+      method: "",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  };
+
+  const createVehicle = (payload: Vehicle) => {
+    const sendRequest = async () => {
+      try {
+        setError("");
+        setLoading(true);
+        let httpOptions = {
+          ...getHttpOptions(),
+          body: JSON.stringify(payload),
+        };
+        httpOptions.method = "POST";
+        const response = await fetch(url, httpOptions);
+        await response.json();
+        setLoading(false);
+        setCallFetch(true);
+      } catch (error) {
+        setError(`Falha ao deletar veículo ${error}`);
+        setLoading(false);
+      }
+    };
+    sendRequest();
+  };
+
+  const deleteVehicle = (id: number) => {
+    const sendRequest = async () => {
+      try {
+        setError("");
+        setLoading(true);
+        let httpOptions = { ...getHttpOptions() };
+        httpOptions.method = "DELETE";
+        const parsedUrl = `${url}/${id}`;
+        const response = await fetch(parsedUrl, httpOptions);
+        await response.json();
+        setLoading(false);
+        setCallFetch(true);
+      } catch (error) {
+        setError(`Falha ao deletar veículo ${error}`);
+        setLoading(false);
+      }
+    };
+    sendRequest();
+  };
+
+  useEffect(() => {
+    const getVehicles = async () => {
+      try {
+        setLoading(true);
+        let httpOptions = { ...getHttpOptions() };
+        httpOptions.method = "GET";
+        const response = await fetch(url, httpOptions);
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(`Falha ao consultar veículos ${error}`);
+        setLoading(false);
+        setData([]);
+      }
+    };
+    getVehicles();
+  }, [url, callFetch]);
+
+  return { data, loading, error, createVehicle, deleteVehicle };
+};
+
+export default useRequest;
